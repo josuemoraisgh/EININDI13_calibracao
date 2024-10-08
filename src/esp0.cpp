007385d4c06467d09a1f94af471ee17bbbd7be9d
Josué Silva de Morais <josuemorais@gmail.com>
2024-10-08 12:19:59 -0300

    refactor esp0.cpp

--- src/esp0.cpp
+++ src/esp0.cpp
@@ -4,7 +4,7 @@
 #define TRMNivel def_pin_R4a20_1
 #define MOTBomba def_pin_RELE
 #define CHNivel def_pin_D1
-#define POSValvula 0
+#define IdCanalValvula 0
 
 bool tripValue = false;
 
@@ -16,7 +16,7 @@ void monitoraPOT(void)
     delayPOT.repeat();
 
     const uint16_t vlPOT1 = analogRead(def_pin_POT1);
-    ledcWrite(POSValvula, vlPOT1);
+    ledcWrite(IdCanalValvula, vlPOT1);
     IIKit.disp.setText(2, ("P1:" + String(100.0 * vlPOT1 / 4096.0)).c_str());
     IIKit.WSerial.plot("vlPOT1", vlPOT1);
   }
@@ -36,7 +36,7 @@ void monitora4A20(void)
     if (++count >= 20)
     {
       vlR4a20_1 = vlR4a20_1 / count;
-      IIKit.disp.setText(3, ("T1:" + String(0.00492273*vlR4a20_1 + 0.71001823)).c_str());
+      IIKit.disp.setText(3, ("T1:" + String(vlR4a20_1)).c_str());
       IIKit.WSerial.plot("vlR4a20_1", vlR4a20_1);
       count = 0;
       vlR4a20_1 = 0.0;
@@ -63,9 +63,9 @@ void setup()
   pinMode(def_pin_D4, OUTPUT);
   attachInterrupt(CHNivel, trip_func, CHANGE);
 
-  ledcAttachPin(def_pin_W4a20_1, POSValvula); // Atribuimos o pino def_pin_W4a20_1 ao canal POSValvula.
-  ledcSetup(POSValvula, 19000, 12);           // Atribuimos ao canal 0 a frequencia de 78kHz com resolucao de 10bits.
-  ledcWrite(POSValvula, 0);                   // Escrevemos um duty cycle de 0% no canal 0.
+  ledcAttachPin(def_pin_W4a20_1, IdCanalValvula); // Atribuimos o pino def_pin_W4a20_1 ao canal POSValvula.
+  ledcSetup(IdCanalValvula, 19000, 12);           // Atribuimos ao canal 0 a frequencia de 78kHz com resolucao de 10bits.
+  ledcWrite(IdCanalValvula, 0);                   // Escrevemos um duty cycle de 0% no canal 0.
 
   IIKit.rtn_1.onValueChanged([](uint8_t status) {
       if(!tripValue) {
