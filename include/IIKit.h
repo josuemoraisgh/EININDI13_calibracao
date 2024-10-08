edd9c1f4a553d12f67503af05d0fa64cae2ce8d0
Josué Silva de Morais <josuemorais@gmail.com>
2024-10-08 10:13:48 -0300

    refactor iikit.h

--- include/IIKit.h
+++ include/IIKit.h
@@ -5,8 +5,8 @@
 #include <ESPmDNS.h>
 #include <EEPROM.h>
 #include <WiFi.h>
-#include <OTA.h>
 
+#include "services\OTA.h"
 #include "services\WSerial_c.h"
 #include "services\display_c.h"
 #include "services\wifimanager_c.h"
@@ -99,7 +99,7 @@ inline void IIKit_c::setup()
         WSerial.print("\nWifi running - IP:");
         WSerial.println(WiFi.localIP());
         disp.setFuncMode(false);
-        disp.setText(1, WiFi.localIP().toString().c_str());
+        disp.setText(1, (WiFi.localIP().toString()+" ID:"+String(idKit[0])).c_str());
         disp.setText(2, DDNSName);
         disp.setText(3, "UFU Mode");
         delay(50);
@@ -108,9 +108,9 @@ inline void IIKit_c::setup()
         errorMsg("Wifi  error.\nAP MODE...", false);
 
     /************** Starting OTA *************/
-    OTA::setup(DDNSName);// OTA tem que ser depois do wifi e wifiManager
+    OTA::start(DDNSName);// OTA tem que ser depois do wifi e wifiManager
     /*** Starting Telnet Mode in WSerial ****/
-    startWSerial(&WSerial,4000);
+    startWSerial(&WSerial,4000+String(idKit[0]).toInt());
     /********** POTENTIOMETERS GPIO define *****/
     pinMode(def_pin_POT1, ANALOG);
     pinMode(def_pin_POT2, ANALOG);
